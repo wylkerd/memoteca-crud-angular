3767198ae683b2e34d3e3f883620de7d0af88f6d
wylkerd <wylkerd@hotmail.com>
2023-02-16 18:21:38 -0300

    comentários

--- src/app/components/pensamentos/pensamento.service.ts
+++ src/app/components/pensamentos/pensamento.service.ts
@@ -12,3 +12,18 @@ export class PensamentoService {
   constructor(private http: HttpClient) { }
 
 }
+
+/*
+  - O Objetivo de um serviço é organizar e compartilhar 
+  regras de negócios, dados e métodos para diferentes 
+  componentes de uma aplicação por meio da injeção de dependência.
+
+  - De forma mais técnica, dependências são serviços, objetos, funções ou até mesmo um 
+  valor que uma classe necessita para desempenhar sua função. No nosso exemplo, 
+  a classe A pode ela mesma ser responsável por criar uma instância da classe B, ou então, 
+  essa dependência pode ser passada para ela, ou melhor dizendo, ser injetada nela. 
+  Esse processo recebe o nome de injeção de dependência.
+
+  De acordo com a documentação do Angular, injeção de dependência é um padrão de projeto 
+  no qual uma classe solicita dependências de fontes externas ao invés de criá-las.
+*/
