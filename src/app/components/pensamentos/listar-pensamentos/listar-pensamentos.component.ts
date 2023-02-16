7b19e1f852bcd5ea1e3b3ca099da68653d759ab6
wylkerd <wylkerd@hotmail.com>
2023-02-16 17:37:34 -0300

    NgClass sendo utilizado

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -8,16 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class ListarPensamentosComponent implements OnInit {
 
   listaPensamentos = [
-    // {
-    //   conteudo: 'Passo informações para o Componente filho',
-    //   autoria: 'Componente Pai',
-    //   modelo: 'modelo3'
-    // },
-    // {
-    //   conteudo: 'Minha propriedade é decorada com @Input()',
-    //   autoria: 'Componente filho',
-    //   modelo: 'modelo2'
-    // },
+    {
+      conteudo: 'Passo informações para o Componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input() Minha propriedade é decorada com @Input() Minha propriedade é decorada com @Input() Minha propriedade é decorada com @Input() Minha propriedade é decorada com @Input() Minha propriedade é decorada com @Input() Minha propriedade é decorada com @Input()',
+      autoria: 'Componente grande',
+      modelo: 'modelo1'
+    },
   ];
 
   constructor() { }
