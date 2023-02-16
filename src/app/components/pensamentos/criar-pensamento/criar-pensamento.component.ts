b9615db19207082d6c778e80b025a1ebc58d4339
wylkerd <wylkerd@hotmail.com>
2023-02-16 14:42:28 -0300

    Two-way Data Binding

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -13,7 +13,7 @@ export class CriarPensamentoComponent implements OnInit {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
-    modelo: ''
+    modelo: 'modelo1'
   }
 
   constructor() { }
