ae05eb7776502e495dc40aa2ce893320075fe473
wylkerd <wylkerd@hotmail.com>
2023-02-16 17:29:41 -0300

    *ngIf()

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -8,16 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ListarPensamentosComponent implements OnInit {
 
   listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o Componente filho',
-      autoria: 'Componente Pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
+    // {
+    //   conteudo: 'Passo informações para o Componente filho',
+    //   autoria: 'Componente Pai',
+    //   modelo: 'modelo3'
+    // },
+    // {
+    //   conteudo: 'Minha propriedade é decorada com @Input()',
+    //   autoria: 'Componente filho',
+    //   modelo: 'modelo2'
+    // },
   ];
 
   constructor() { }
