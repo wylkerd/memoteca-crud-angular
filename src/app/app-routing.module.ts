ee43cbfabd066ca835f93efe8bcddbb295555815
wylkerd <wylkerd@hotmail.com>
2023-02-16 15:21:45 -0300

    Configuração de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentosComponent } from './components/pensamentos/listar-pensamentos/listar-pensamentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // Rota inicial,
+    redirectTo: 'listarPensamentos',
+    pathMatch: 'full' // para ler toda url e redirecionar
+  },
+  {
+    path: 'criarPensamento', // Dando nome à rota
+    component: CriarPensamentoComponent,
+  },
+  {
+    path: 'listarPensamentos', // Dando nome à rota
+    component: ListarPensamentosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
